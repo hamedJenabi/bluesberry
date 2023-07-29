eaf58c31b3ed44ce568650a0362c2033e86cfd5f
Hamed <hamed.jenabi@gmail.com>
2023-07-30 00:22:07 +0200

    fix

--- app/sales/page.js
+++ app/sales/page.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.scss";
-import Header from "./components/Header/Header";
+import Header from "../components/Header/Header";
 
 export default async function Home() {
   return (
